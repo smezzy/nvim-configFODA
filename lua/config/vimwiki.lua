5280c65f90137d3b0584dd7e22d7f023f7ba3a09
smezzy <yagamiztc123@gmail.com>
2023-07-14 01:36:14 -0300

    update todo keybiding

--- lua/config/vimwiki.lua
+++ lua/config/vimwiki.lua
@@ -1,15 +1,34 @@
-
 vim.cmd([[
-  augroup VimwikiTabwidth
-    autocmd!
-    autocmd FileType vimwiki setlocal tabstop=2 shiftwidth=2
-  augroup END
+    augroup VimwikiTabwidth
+        autocmd!
+        autocmd FileType vimwiki setlocal tabstop=2 shiftwidth=2
+    augroup END
 ]])
 
-local opts = { silent = true, noremap = true }
-vim.keymap.set('n', '<Leader><space>', [[:VimwikiToggleListItem<CR>:normal! A @done `(<C-R>=strftime('%d-%m-%Y %H:%M')<CR>)`<CR>]], opts)
+-- cool prefixes
+
+-- local prefix = "@done"
+-- local prefix = "cu"
+-- local prefix = "@at"
+local prefix = ""
+
+local function toggleVimwikiListItem()
+    vim.cmd('VimwikiToggleListItem')
+
+    local line = vim.fn.getline('.')
+    if line:match('.*`'..prefix..' [^`]*') then
+        line = line:gsub(' `'..prefix..'[^`]*`', ''):gsub('`%([^)]*%)`', ''):gsub('%s*$', ''):gsub('`$', '')
+    else
+        line = line:gsub('%s*$', '') .. ' `'..prefix..' (' .. os.date('%d-%m-%Y %H:%M') .. ')`'
+    end
+    vim.fn.setline('.', line)
+end
+
+vim.keymap.set('n', '<Leader><space>', toggleVimwikiListItem, {silent = true, noremap = true})
+
 
 -- Highlight completed todo items
-vim.g.vimwiki_hl_cb_checked = 0
+-- vim.api.nvim_command("highlight link VimwikiCheckBoxDone Todo")
+vim.g.vimwiki_hl_cb_checked = 1
 vim.g.vimwiki_conceal_pre = 1
 vim.g.automatic_nested_syntaxes = 1
